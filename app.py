9db8061410c55ccf3812b0c35a16667ed117ef29
Andrea Dall'Alba <HGS-Andry@users.noreply.github.com>
2019-06-21 14:49:27 +0200

    insert some buttons

--- app.py
+++ app.py
@@ -104,6 +104,16 @@ def getordine():
     # gestiamo il carrello
     return render_template('carrello.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html')
+
+@app.errorhandler(403)
+def page_Forbidden(e):
+    # note that we set the 403 status explicitly
+    return render_template('403.html')
+
 #################################
 ##  Funzioni varie 
 #################################
